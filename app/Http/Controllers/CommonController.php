77a840757b4e841c6856ae5d3b8e31a77eeaab33
blacksun <18982821376@sina.cn>
2016-11-04 16:34:59 +0800

    公共控制器

---
+++ app/Http/Controllers/CommonController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CommonController extends Controller
+{
+    public function timeline(){
+        list($limit,$skip) = paginate(rq('page'));
+        $questions = question_ins()
+            ->limit($limit)
+            ->skip($skip)
+            ->orderBy('created_at','desc')
+            ->get();
+        $answers = answer_ins()
+            ->limit($limit)
+            ->skip($skip)
+            ->orderBy('created_at','desc')
+            ->get();
+        $data = $questions->merge($answers);
+        $data = $data->sortByDesc(function($item){
+            return $item->created_at;
+        });
+        $data = $data->values()->all();
+        return arrayChange(1,'',$data);
+    }
+}
